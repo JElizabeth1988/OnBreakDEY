68a76a100163e906925ec867e8e41f797a3ec382
JElizabeth1988 <johana.elizabeth1988@gmail.com>
2019-04-13 01:05:55 -0400

    daoCliente

--- Vista/WpfCliente.xaml.cs
+++ Vista/WpfCliente.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
 using BibliotecaClase;
+using BibliotecaControlador;
 
 
 using MahApps.Metro.Controls;
@@ -26,6 +27,7 @@ namespace Vista
     /// </summary>
     public partial class WpfCliente : MetroWindow
     {
+        DaoCliente dao;
 
         public WpfCliente()
         {
@@ -68,7 +70,42 @@ namespace Vista
 
         private void btnGuardar_Click(object sender, RoutedEventArgs e)
         {
-           
+            try
+            {
+                String rut = txtRut.Text;
+                String razonSocial = txtRazon.Text;
+                String nombreContacto = txtNombre.Text;
+                String mail = txtEmail.Text;
+                String direccion = txtDireccion.Text;
+                int telefono = int.Parse(txtTelefono.Text);
+                ActividadEmpresa actividad = (ActividadEmpresa)cbActividad.SelectedItem;
+                TipoEmpresa empresa = (TipoEmpresa)cbTipo.SelectedItem;
+                Cliente c = new Cliente()
+                {
+                    Rut = rut,
+                    RazonSocial = razonSocial,
+                    NombreContacto = nombreContacto,
+                    Mail = mail,
+                    Direccion = direccion,
+                    Telefono = telefono,
+                    Actividad = actividad,
+                    Empresa = empresa
+
+                };
+                bool resp = dao.Agregar(c);
+                MessageBox.Show(resp ? "Grabo" : "NoGrabo");
+
+            }
+            catch (ArgumentException exa)
+            {
+                MessageBox.Show(exa.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error de ingreso de datos");
+
+                throw;
+            }
         }
     }
 }
