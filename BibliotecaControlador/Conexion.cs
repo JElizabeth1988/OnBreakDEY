ac9c3e7bbedab38734fc1ffe6aba1f37dbfc8816
JElizabeth1988 <johana.elizabeth1988@gmail.com>
2019-05-10 15:06:47 -0400

    creacion clase conexion incompleta

---
+++ BibliotecaControlador/Conexion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace BibliotecaControlador
+{
+    public class Conexion
+    {
+        private static SqlConnection cone;
+        //falta data source catalogo usuario y contraseña
+        private String _cadena = "Data Source=;Initial Catalog=;Persist Security Info=True;User ID=;Password=";
+
+        public Conexion()
+        {
+            if (cone == null)
+            {
+                cone = new SqlConnection(_cadena);
+            }
+        }
+
+        public SqlConnection GetConexion()
+        {
+            return cone;
+        }
+    }
+}
