cdd2a6314f318a60beca4fb46fe2130acfd47063
hidalgog <hidalgog@users.noreply.github.com>
2019-06-24 17:54:40 -0400

    Clase Coffe Break,Clase Tipo ambientación

--- BibliotecaControlador/CofeeBreak.cs
+++ BibliotecaControlador/CofeeBreak.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BibliotecaDALC;
 
-namespace BibliotecaControlador
+namespace BibliotecaNegocio
 {
     public class CofeeBreak
     {
@@ -25,6 +26,45 @@ namespace BibliotecaControlador
             set { _vegetariana = value; }
         }
 
+        private OnBreakEntities bdd = new OnBreakEntities();
+        public CofeeBreak()
+        {
+                
+        }
 
+        public bool Read()
+        {
+            try
+            {
+                BibliotecaDALC.CoffeeBreak co = bdd.
+                    CoffeeBreak.First(c => c.Numero == Numero);
+                Vegetariana = co.Vegetariana;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public List<CoffeeBreak> ReadAll()
+        {
+            try
+            {
+                List<CoffeeBreak> lista = new List<CoffeeBreak>();
+                var lista_co_bdd = bdd.CoffeeBreak.ToList();
+                foreach (BibliotecaDALC.CoffeeBreak item in lista_co_bdd)
+                {
+                    CoffeeBreak co = new CoffeeBreak();
+                    co.Numero = item.Numero;
+                    co.Vegetariana = item.Vegetariana;
+                    lista.Add(co);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
     }
 }
--- BibliotecaControlador/TipoAmbientacion.cs
+++ BibliotecaControlador/TipoAmbientacion.cs
@@ -3,8 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BibliotecaDALC;
 
-namespace BibliotecaControlador
+namespace BibliotecaNegocio
 {
     public class TipoAmbientacion
     {
@@ -24,8 +25,47 @@ namespace BibliotecaControlador
             set { _descripcion = value; }
         }
 
-      
+        private OnBreakEntities bdd = new OnBreakEntities();
 
+        public TipoAmbientacion()
+        {
+
+        }
+
+        public bool Read()
+        {
+            try
+            {
+                BibliotecaDALC.TipoAmbientacion tipo = bdd.
+                    TipoAmbientacion.First(t => t.IdTipoAmbientacion == IdTipoAmbientacion);
+                Descripcion = tipo.Descripcion;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+        public List<TipoAmbientacion> ReadAll()
+        {
+            try
+            {
+                List<TipoAmbientacion> lista = new List<TipoAmbientacion>();
+                var lista_tipo_bdd = bdd.TipoAmbientacion.ToList();
+                foreach (BibliotecaDALC.TipoAmbientacion item in lista_tipo_bdd)
+                {
+                    TipoAmbientacion tipo = new TipoAmbientacion();
+                    tipo.IdTipoAmbientacion = item.IdTipoAmbientacion;
+                    tipo.Descripcion = item.Descripcion;
+                    lista.Add(tipo);
+                }
+                return lista;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
 
     }
 }
